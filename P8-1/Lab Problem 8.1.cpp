2613bca1d8961289a09709cc19e51aee588cb513
Caleb Collert <c-collert.1@onu.edu>
2024-03-28 09:02:05 -0400

    Completed countCharacter function.

--- P8-1/Lab Problem 8.1.cpp
+++ P8-1/Lab Problem 8.1.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+int countCharacter(string str);
+
+int main() {
+
+	cout << countCharacter("hello world!");
+
+}
+
+int countCharacter(string str) {
+
+	return str.length();
+}
